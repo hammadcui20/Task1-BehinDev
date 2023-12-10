2cd6acd244ce57c259ea8203b66043d8c548de53
Hammad Hussain <hammadasmat@gmail.com>
2023-12-10 18:31:25 +0500

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Outputs:
+Bootstrap Project Built by Dilawar khan.
+
+
+![screencapture-dilawarkhaninfo-github-io-sis-project-2023-11-05-12_54_47](https://github.com/Dilawarkhaninfo/sis_project/assets/130894444/7f2d1f37-3010-4a19-8282-4a6de9fa7e1b)
---
+++ js/myScript.js
@@ -0,0 +1,47 @@
+// AOS.init();
+
+
+// menu js start
+ window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.querySelector(".navbar").style.background = "#000";
+        document.querySelector(".navbar").classList.add('active');
+        document.querySelector(".navbar").style.height= '70px';
+        document.querySelector(".sizelogo").style.width = '80px';
+
+    } else {
+        document.querySelector(".navbar").style.background = "transparent";
+        document.querySelector(".navbar").classList.remove('active');
+        document.querySelector(".navbar").style.height= 'auto';
+        document.querySelector(".sizelogo").style.width = '100px';
+
+    }
+}
+
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+// menu js end
+
+
+
+
+
+
+
+
+
+
+
+
+
